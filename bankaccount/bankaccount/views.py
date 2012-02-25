35979f97e0ff7e012e3341ad6421a12a223dc7ad
yamionp <yami@crimsondream.jp>
2012-02-25 15:09:47 +0900

    view関数をルートに紐付け&テンプレートファイルをちゃんと指定した

--- bankaccount/bankaccount/views.py
+++ bankaccount/bankaccount/views.py
@@ -1,18 +1,22 @@
 from pyramid.httpexceptions import HTTPFound
+from pyramid.view import view_config
 from models import NotEnoughFunds
 
 def my_view(request):
     return {'project':'bankaccount'}
 
+@view_config(route_name='home', renderer="index.mak")
 def home(request):
     bankaccount = request.context.get_bankaccount()
     return dict(bankaccount=bankaccount)
 
+@view_config(route_name='deposit')
 def deposit(request):
     bankaccount = request.context.get_bankaccount()
     bankaccount.deposit(int(request.params['amount']))
     return HTTPFound(location=request.route_url('home'))
 
+@view_config(route_name='withdraw')
 def withdraw(request):
     bankaccount = request.context.get_bankaccount()
     try:
